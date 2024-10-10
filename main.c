c4a71e3666dd08ade141aeddcc21d2820627675c
Richard Velasco <velascor@gmail.com>
2024-10-09 22:04:51 -0700

    added video streaming from usb camera to hdmi monitor, displaying text overlay of gain and direction of arrival of signal

--- main.c
+++ main.c
@@ -7,11 +7,62 @@
 #include <signal.h>
 #include <unistd.h>
 #include <termios.h>
+#include <glib.h>
+#include <gst/gst.h>
 //#define PCM_DEVICE "plughw:1,0"
 //#define CAPTURE_DURATION 1
 //#define SAMPLE_RATE 44100    // Sampling rate in Hz
 //#define PCM_FORMAT SND_PCM_FORMAT_S16_LE  // PCM format (16-bit little-endian)
 //#define CAPTURE_DURATION 1
+
+static GMainLoop *loop;
+static GstElement *textoverlay;
+static int gain = 1;
+static int doa = 0;
+
+static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data) {
+    GMainLoop *loop = (GMainLoop *)data;
+
+    switch (GST_MESSAGE_TYPE(msg)) {
+        case GST_MESSAGE_EOS:
+            g_print("End of stream\n");
+            g_main_loop_quit(loop);
+            break;
+        case GST_MESSAGE_ERROR: {
+            gchar *debug;
+            GError *error;
+            gst_message_parse_error(msg, &error, &debug);
+            g_free(debug);
+            g_printerr("Error: %s\n", error->message);
+            g_error_free(error);
+            g_main_loop_quit(loop);
+            break;
+        }
+        default:
+            //g_print("Some Message Recevied...\n");
+            break;
+    }
+
+    return TRUE;
+}
+
+void *update_text(void *data) {
+    
+    gchar text[100];
+
+    while (1) {
+        // Create a dynamic text
+        snprintf(text, sizeof(text), "Gain: %d, DOA: %d", gain, doa);
+        
+        // Update the textoverlay element
+        g_object_set(G_OBJECT(textoverlay), "text", text, NULL);
+        
+        // Sleep for 1 second before updating again
+        sleep(1);
+    }
+
+    return NULL;
+}
 
 //set to noncanonical mode to capture key presses
 void set_noncanonical_mode(){
@@ -42,20 +93,17 @@ char get_key_press(){
 volatile sig_atomic_t stop = 0;
 
 volatile int running = 1;
+volatile int thread_running = 1;
 
 void handle_sigint(int sig){
-    //printf("Caught signal %d, cleaning up...\n",sig);
+    printf("Caught Interrupt signal %d, cleaning up...\n",sig);
+    g_main_loop_quit(loop);
     //running = 0;
     stop = 1;
 }
 
-int main(int argc, char *argv[]) {
-
-    signal(SIGINT, handle_sigint);
-
-    set_noncanonical_mode();
-
-    char key_press;
+void *audio_thread_main(void *arg){
+        char key_press;
 
     char *device_names[MAX_DEVICES] = {0};
     pthread_t mic_threads[7] = {0};
@@ -73,15 +121,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
-//********************************************* */
-//Testing Above
-///////////////////////////////////////////////////s
-
-    if(argc > 1){
-        printf("No arguments needed\n");
-        return 0;
-    }
     printf("micarray started...\n");
     int myArraySize = sizeof(mic_array_t);
     printf("micarray size is: %d\n",myArraySize);
@@ -99,14 +138,14 @@ int main(int argc, char *argv[]) {
 
     if(open_and_configure_capture_devices(myArray) < 0){
         printf("Error open and configure\n");
-        return -1;
+        return (void *)-1;
     }
 
     setup_output_pcm(&playback_handle);
     
     mixed_buffer = malloc(FRAMES * PLAYBACK_FRAMES);
-    int gain = 1;
-    int doa = 0;
+    gain = 1;
+    doa = 0;
     while(running && !stop){
                 key_press = get_key_press();
                 if (key_press == 'q')
@@ -148,7 +187,7 @@ int main(int argc, char *argv[]) {
                 }
                 else
                 {
-                    //      printf("Key pressed is %c\n",key_press);
+                    //printf("Key pressed is %c\n",key_press);
                 }
 
         memset(mixed_buffer,0,FRAMES*PLAYBACK_FRAMES);
@@ -162,53 +201,115 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    restore_terminal_mode();
+    
     close_capture_devices(myArray);
-    snd_pcm_close(playback_handle);
-    return 0;
-    //if(open_file_and_record(myArray[0]) < 0){
-    //    printf("Error recording\n");
-    //    return -1;
-    //}
-/*
-    for(int i = 0; i < MAX_DEVICES; i++){
-    myArray[i]->running = 1;
-    pthread_create(&myArray[i]->thread,NULL,open_file_and_record_thread,myArray[i]);
-    //open_file_and_record(myArray[i]);
-    }
-
-    //pthread_join(myArray[0]->thread,NULL);
-
-    int threads_complete = 0;
-
-    for(int i = 0 ; i < MAX_DEVICES ; i++){
-        threads_complete += myArray[i]->running;
-    }
-    int running_threads = MAX_DEVICES;
-    while(running_threads){
-        //printf("running threads: %d\n",running_threads);
-        for(int i = 0 ; i < MAX_DEVICES ; i++){
-            if(myArray[i]->running == 0){
-                printf("thread is running: %d\n",myArray[i]->running);
-                myArray[i]->running = -1;
-                pthread_join(myArray[i]->thread,NULL);
-                close_capture_device(myArray[i]);
-                free(device_names[i]);
-		        running_threads -= 1;
-                //free(myArray[i]);
-            }
-        }
-
-    }
-
-    if(0){//close_capture_devices(myArray) < 0){
-        printf("Error closing devices\n");
-        return -1;
-    }
-    for(int i = 0 ; i < MAX_DEVICES ; i++){
-        //free(device_names[i]);
+    for(int i = 0 ; i < MAX_DEVICES; i++){
         free(myArray[i]);
     }
-    */
+    snd_pcm_close(playback_handle);
+    thread_running = 0;
+    g_main_loop_quit(loop);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+
+    signal(SIGINT, handle_sigint);
+
+    pthread_t audio_thread, text_thread;
+
+    set_noncanonical_mode();
+
+    GstElement *pipeline, *source, *convert, *sink;
+    GstBus *bus;
+    GstCaps *caps;
+  
+
+    // Initialize GStreamer
+    gst_init(&argc, &argv);
+
+    // Create the main loop
+    loop = g_main_loop_new(NULL, FALSE);
+
+    // Create the elements
+    source = gst_element_factory_make("v4l2src", "source");
+    convert = gst_element_factory_make("videoconvert", "convert");
+    textoverlay = gst_element_factory_make("textoverlay", "textoverlay");
+    sink = gst_element_factory_make("autovideosink", "sink");
+
+    if (!source || !convert || !textoverlay || !sink) {
+        g_printerr("One element could not be created. Exiting.\n");
+        return -1;
+    }
+
+        // Define the caps for the source (v4l2src)
+    caps = gst_caps_new_simple("video/x-raw",
+                               "width", G_TYPE_INT, 640,
+                               "height", G_TYPE_INT, 480,
+                               "framerate", GST_TYPE_FRACTION, 30, 1,
+                               "format", G_TYPE_STRING, "YUY2",
+                               "interlace-mode", G_TYPE_STRING, "progressive",
+                               "colorimetry", G_TYPE_STRING, "2:4:16:1",
+                               NULL);
+
+        // Set the custom text overlay properties
+    g_object_set(G_OBJECT(textoverlay),
+                 "text", "Your Custom Text",   // The text to overlay
+                 "valignment", 0,              // Align the text to the top (0 = top, 1 = center, 2 = bottom)
+                 "halignment", 0,              // Align the text to the left (0 = left, 1 = center, 2 = right)
+                 "font-desc", "Sans, 24",      // Font description
+                 NULL);
+
+// Create the empty pipeline
+    pipeline = gst_pipeline_new("video-pipeline");
+
+    // Build the pipeline
+    gst_bin_add_many(GST_BIN(pipeline), source, convert, textoverlay, sink, NULL);
+
+    // Link the source with caps to videoconvert
+    if (!gst_element_link_filtered(source, convert, caps)) {
+        g_printerr("Source and convert could not be linked with caps. Exiting.\n");
+        gst_caps_unref(caps);
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Link the rest of the pipeline
+    if (!gst_element_link_many(convert, textoverlay, sink, NULL)) {
+        g_printerr("Convert, textoverlay, and sink could not be linked. Exiting.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Free caps after usage
+    gst_caps_unref(caps);
+
+    // Set the pipeline to "playing" state
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+
+    pthread_create(&audio_thread,NULL,audio_thread_main,NULL);
+
+    // Set up a message handler on the bus
+    bus = gst_element_get_bus(pipeline);
+    gst_bus_add_watch(bus, bus_call, loop);
+    gst_object_unref(bus);
+
+    pthread_create(&text_thread, NULL, update_text, NULL);
+    // Run the main loop
+    g_main_loop_run(loop);
+
+    pthread_join(audio_thread,NULL);
+
+    // Clean up after exiting the loop
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+    g_main_loop_unref(loop);
+
+    //pthread_create(&audio_thread,NULL,audio_thread_main,NULL);
+    //pthread_join(audio_thread,NULL);
+    restore_terminal_mode();
+    return 0;
+
  
 }
